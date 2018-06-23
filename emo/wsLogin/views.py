29b97ac0564b679afbfa6a6fd9a03f383460ff24
15331033 <1179653327@qq.com>
2018-06-23 17:43:41 +0800

    wxLogin

--- emo/wsLogin/views.py
+++ emo/wsLogin/views.py
@@ -19,13 +19,13 @@ def login(request):
     except BaseException:
         user = WxUser()
         user.sex = random.randint(1,2)
-        user = user.save()
+        user.save()
         user.session_id = session 
         user.nickname = 'user'+str(user.id)
         user.province = 'province'+str(user.id)
         user.city = 'city'+str(user.id)
         user.country = 'country'+str(user.id)
         user.headimgurl = 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1529751764221&di=7b77ae9d0598a1d996abbd83ff63b7b5&imgtype=0&src=http%3A%2F%2Fupload.mnw.cn%2F2018%2F0324%2F1521876947374.png'
-        user = user.save()
+        user.save()
         responseData = {'openid': user.id, 'nickname': user.nickname, 'sex': user.sex, 'province': user.province, 'city': user.city, 'country': user.country, 'headimgurl': user.headimgurl, 'privilege': '超级加倍', 'unionid': '3838438'}
         return HttpResponse(json.dumps(responseData, ensure_ascii=False), content_type="application/json")
